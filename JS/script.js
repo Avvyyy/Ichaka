7e91d7758eebe9fe1e3df9bac20775511b6561ad
Avvyyy <uzochukwu.favour310@gmail.com>
2023-01-13 10:57:30 -0800

    I worked more on the culture tab in this commit

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+function externalLink() {
+  for (
+    var link = document.getElementsByClassName("externalLink"), a = 0;
+    a < c.length;
+    a++
+  ) {
+    var b = c[a];
+    b.getAttribute("href") &&
+      b.hostname !== location.hotsname &&
+      (b.traget = "_blank");
+  }
+}
+
+externalLink();
