04a45b4d740fb2195c84c03542b754bbd57c207c
Avvyyy <uzochukwu.favour310@gmail.com>
2023-02-14 13:11:13 -0800

    Implementation of AJAX

--- JS/script.js
+++ JS/script.js
@@ -1,33 +1,59 @@
-// const apiName = "https://igboapi.com/api/v1/words?keyword="+word+"&dialects=true&examples=true"     
-// const apiKey = 'ba00fc6f-8c74-4be8-a26f-86c54f312de4'
-// const word = document.getElementById('searchBox').value
 
-// curl -X 'GET' \
-//   'https://undefined/api/v1/words?keyword=goat&page=1&range=1%2C1&strict=false&dialects=false&examples=true&pronunciation=false&nsibidi=false' \
-//   -H 'accept: application/json' \
-//   -H 'X-API-Key: b66e7848-5712-461c-a17e-85b6b2ffcda0'
 
-// let http = new XMLHttpRequest();
-// http.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//        var response = JSON.parse(http.responseText);
-//        document.getElementsByClassName('Meanings').innerHtml = response
-//     }
-// }
+document.getElementById("dictBox").addEventListener("submit", fetchData);
 
-// http.open("GET" , apiName, true );
-// http.send();
+function fetchData(e) {
 
-document.getElementsByTagName('body').addEventListener('load', fetchData);
-const fetchData = () =>{
-    let http = new XMLHttpRequest();
-    http.open = ('GET', 'https://igboapi.com/api/v1/words?keyword=boar&dialects=true&examples=true', true);
-    http.onload = function(){
-        if (this.status === 200) {
-        console.log(this.responseText);
+    let userInput = document.getElementById("searchBox").value;
 
-        http.send;
+  e.preventDefault();
+  let xhttp = new XMLHttpRequest();
+xhttp.open(
+    "GET",
+    "https://igboapi.com/api/v1/words?keyword=" + userInput + "&strict=false&dialects=true&examples=true&isStandardIgbo=true&pronunciation=false&nsibidi=false",
+    true
+);
+
+xhttp.onload = function () {
+    if (this.status == 200) {
+        let data = JSON.parse(this.responseText);
+
+        for (let i = 0; i < data.length; i++) {
+            let record = data[i];
+            let meanings = record["definitions"];
+            let examples = record["examples"];
+            // let word = record["word"];
+            let dialects = record["dialects"];
+
+            document.querySelector('.meanings').innerHTML = 'Meanings:';
+            for (let j = 0; j < meanings.length; j++) {
+              document.querySelector('.meanings').innerHTML += "<br> -`" + meanings[j];
+            }
+
+           
+
+            document.querySelector('.examples').innerHTML = "Examples :";
+            for (let k = 0; k < examples.length; k++) {
+                document.querySelector('.examples').innerHTML +=   "<br> - " + examples[k]["igbo"] + ": " + examples[k]["english"];
+            }
+
+            document.querySelector('.dialects').innerHTML= "Dialects :";
+            for (let l in dialects) {
+                let dialect = dialects[l];
+                document.querySelector('.dialects').innerHTML += "<br> - " + dialect["word"] + ": " + dialect["dialects"].join(", ");
+            }
+        }
     }
-        
-    }
+};
+
+xhttp.setRequestHeader("X-API-Key", "465ef4c9-1935-42be-a5d7-5d7d3e299f3a");
+xhttp.setRequestHeader("Content-type", "application/json");
+xhttp.setRequestHeader("Accept", "application/json");
+
+xhttp.send();
+
 }
+
+
+const backspace = document.getElementById('closeButton');
+// backspace.onclick => 
